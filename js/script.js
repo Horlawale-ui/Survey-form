54a0a6d1e17780330c9d732298d1277c17431fb5
Horlawale-ui <53876892+Horlawale-ui@users.noreply.github.com>
2021-06-02 16:19:12 +0100

    Testing phase

--- js/script.js
+++ js/script.js
@@ -15,27 +15,51 @@ for (var i =0; i < arr.length; i++)
     select.insertBefore(option, select.lastChild);
 }
 
-function checkValues() {
+document.getElementById("btn-1").addEventListener('click', checkFirstValues, false);
+document.getElementById("btn-2").addEventListener('click', checkSecondValues, false);
+
+function checkFirstValues() {
     var age = document.querySelectorAll('input[data-name="age"]'),
     education = document.querySelectorAll('input[data-name="files"]'),
     occupation = document.querySelectorAll('input[data-name="occupation"]'),
     gender = document.getElementById('option-values'),
     genderValue = gender.options[gender.selectedIndex].value.length;
+    selectOption = document.getElementById('select'),
+    selectOptionValue = selectOption.options[selectOption.selectedIndex].value;
     if (genderValue < 7) {
-            occupation.forEach(function (e) {
-                if (e.checked) {
-                    age.forEach(function (e) {
-                        if (e.checked) {
-                            education.forEach(function (e) {
+        for (var i =0; i < arr.length; i++) {
+            if (selectOptionValue === arr[i]) {
+                occupation.forEach(function (e) {
+                    if (e.checked) {
+                        age.forEach(function (e) {
                             if (e.checked) {
-                                var form = document.querySelector('form');
-                                form.classList.add('reveal');
-                        }})
+                                education.forEach(function (e) {
+                                if (e.checked) {
+                                    var form = document.querySelector('form');
+                                    form.classList.add('reveal');
+                            }})
+                    }})
                 }})
-            }})
-    }
-}
+            }
+        }
+}}
 
+function checkSecondValues() {
+    var satisfied = document.querySelectorAll('input[data-name="satisfied"]'),
+        textField = document.getElementById('neccessitate').value,
+        recommend = document.querySelectorAll('input[data-name="yes"]');
+       satisfied.forEach(function(e) {
+           if (e.checked) {
+               if (textField.length > 10) {
+                       recommend.forEach(function (e) {
+                           if (e.checked) {
+                               var form = document.querySelector('form');
+                               form.classList.add('reveal-2');
+                       }})
+                }
+            } 
+    })
+}
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -73,21 +97,37 @@ function submitForm(e){
     let satisfied = document.querySelector('input[name="satisfied"]:checked');
     var necessitate = getInputVal('neccessitate');
     let yes = document.querySelector('input[name="y/n"]:checked');
+    var checkbox = document.querySelector('input[name="known-product"]:checked');
+    var product = document.querySelector('input[name="answer-1"]:checked');
+    var customer = document.querySelector('input[name="answer-2"]:checked');
+    var premium = document.querySelector('input[name="answer-3"]:checked');
+    var claim = document.querySelector('input[name="answer-4"]:checked');
+    var social = document.querySelector('input[name="answer-5"]:checked');
+    var issues = document.querySelector('input[name="answer-6"]:checked');
     var help = getInputVal('help');
+    var rating = document.querySelector('input[name="number"]:checked');
+    var channels = document.querySelector('input[name="choice"]:checked');
+    var improve = getInputVal('improve');
+    var assistance = getInputVal('assistance');
+    var advice = getInputVal('advice');
+    var ideal = document.querySelector('input[name="ideal_options"]:checked');
+
 
     //save message
-    saveMessage(select, select2, employed,age, degree, satisfied, necessitate, yes,help);
+    saveMessage(select, select2, employed,age, degree, satisfied, necessitate, yes, checkbox, product, customer, premium, claim, social, issues, help, rating, channels, improve, assistance, advice, ideal);
 
     //Show alert
-    document.querySelector('.alert-msg').style.display = 'block';
+    document.querySelector('.submitpopup-container').style.display = 'flex';
 
     //Hide Alert After reset
     setTimeout(function(){
-     document.querySelector('.alert-msg').style.display = 'none'
-    },3000);
+     document.querySelector('.submitpopup-container').style.display = 'none'
+    },8000);
 
     //Clear Form
     document.getElementById('contact-form').reset();
+    location.reload();
+
     
 }
 
@@ -96,7 +136,7 @@ function getInputVal(id){
     return document.getElementById(id).value;
 }
 // Save message to firebase
-function saveMessage(select, select2, employed, age, degree, satisfied, necessitate, yes, help){
+function saveMessage(select, select2, employed,age, degree, satisfied, necessitate, yes, checkbox, product, customer, premium, claim, social, issues, help, rating, channels,  improve, assistance, advice, ideal){
     var newMessageRef = messageRef.push();
     newMessageRef.set({
         gender: select.options[select.selectedIndex].value,
@@ -107,6 +147,19 @@ function saveMessage(select, select2, employed, age, degree, satisfied, necessit
         satisfication: satisfied.value,
         necessitate: necessitate,
         recommendation: yes.value,
-        experience: help
+        knownProduct: checkbox.value,
+        productRating: product.value,
+        customerServiceRating: customer.value,
+        premiumRating: premium.value,
+        claimRating: claim.value,
+        socialRating: social.value,
+        issuesrating: issues.value,
+        experience: help,
+        lasacoRating: rating.value,
+        channelMethod: channels.value,
+        waystoImprove: improve,
+        waystoImproveTwo: assistance,
+        waystoImproveThree: advice,
+        ideal: ideal.value
     });
 }
